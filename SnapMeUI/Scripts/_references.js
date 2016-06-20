af9935cc87c524b9bad38ef195d1f02546a780b7
wakawaka54 <soue7@hotmail.com>
2016-06-19 20:58:36 -0500

    Initial Commit
    Initial Commit

---
+++ SnapMeUI/Scripts/_references.js
@@ -0,0 +1,4 @@
+﻿/// <reference path="modernizr-2.6.2.js" /> 
+/// <reference path="bootstrap.min.js" /> 
+/// <reference path="jquery-2.2.4.min.js" /> 
+/// <reference path="Slick/slick.js" />
---
+++ SnapMeUI/Scripts/carousel/carousel.js
@@ -0,0 +1,28 @@
+﻿
+var xPos = 0;
+
+function scroll() {
+    xPos -= 10;
+    var imgs = $(".carousel-img");
+    imgs.css("transform", "translateX(" + xPos + "px)");
+
+    if(xPos <= -screen.width)
+    {
+        xPos = 0;
+    }
+}
+
+function righthover() {
+
+}
+
+function lefthover() {
+
+}
+
+function main()
+{
+
+}
+
+$(document).ready(main);
